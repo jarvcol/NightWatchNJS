01f5432773c0d0e8392b55818b98a84c7ed6da09
Jorge Andres Rojas Valencia <JARV@JARVs-MacBook-Pro.local>
2018-08-03 14:15:02 -0500

    Branch 1 up to move slider

--- pages/carnivalHome.js
+++ pages/carnivalHome.js
@@ -3,11 +3,91 @@ module.exports = function (browser) {
       browser
         .windowMaximize()
         .url('https://www.carnival.com/')
-        .waitForElementVisible('body', 1000);
+        .waitForElementVisible('body', 5000);
       return browser;
     };
     this.verifyPage = function () {
-      browser.assert.title("Cruises | Carnival Cruise Deals: Caribbean, Bahamas, Alaska, Mexico");
+      browser.verify.title("Cruises | Carnival Cruise Deals: Caribbean, Bahamas, Alaska, Mexico");
+    };
+    this.clickSailTo = function () {
+      browser
+      .click('#cdc-destinations')
+      .waitForElementVisible('.cdc-filter-body', 5000);
+    };
+    this.setSailToValue = function(){
+      browser
+      .useXpath()
+      .click("//button[@aria-label='The Bahamas ']")
+      .waitForElementVisible('//article', 5000);
+    };
+    this.clickDuration = function () {
+      browser
+      .useCss()
+      .click('#cdc-durations')
+      .waitForElementVisible('.cdc-filter-body', 5000);
+    };
+    this.setDuration = function(){
+      browser
+      .useXpath()
+      .click("//button[@aria-label='6 - 9 Days ']")
+      .waitForElementVisible('//article', 5000);
+    };
+    this.assertGridView = function(){
+      browser
+      .useCss()
+      .assert.elementPresent("ccl-view-result-grid")
+    };
+    this.clickPricingFilter = function(){
+      browser
+      .useCss()
+      .click('#sfn-nav-pricing')
+      .waitForElementVisible('.rz-pointer.rz-pointer-min', 5000);
+    };
+    this.moveSliderMinToRight = function(){
+      browser
+      .useCss()
+      .getAttribute('#sfn-nav-pricing','aria-expanded', function(result) {
+        if (result.value.toString() == 'false')
+          clickPricingFilter();
+      });
+      var cycle = 20;
+      while(cycle != 0){
+        browser.setValue('.rz-pointer.rz-pointer-min', browser.Keys.RIGHT_ARROW)
+        .waitForElementVisible('article', 5000);
+        cycle = cycle -1 ;
+      };
+    };
+    this.moveSliderMaxToRight = function(){
+      browser
+      .useCss()
+      .getAttribute('#sfn-nav-pricing','aria-expanded', function(result) {
+        if (result.value.toString() == 'false')
+          clickPricingFilter();
+      });
+      var cycle = 20;
+      while(cycle != 0){
+        browser.setValue('.rz-pointer.rz-pointer-max', browser.Keys.LEFT_ARROW)
+        .waitForElementVisible('article', 5000);
+        cycle = cycle -1 ;
+      };
+    };
+    this.clickResetValues = function(){
+      browser
+      .useCss()
+      .assert.elementPresent(".sfp-reset__button")
+    };
+    this.assertSliderFunction = function(){
+      browser
+      .useCss()
+      .waitForElementVisible('.rz-pointer.rz-pointer-min', 5000)
+      .getAttribute('.rz-pointer.rz-pointer-min','aria-valuenow', function(filterValue) {
+        browser
+        .waitForElementVisible('p+.vrgf-price-box__price-info', 5000)
+        .getText('p+.vrgf-price-box__price-info', function(articleValue) {
+          console.log("first "+filterValue.value.toString());
+          console.log("second "+JSON.stringify(articleValue.value));
+        });
+      });
     };
     return this;
   };
--- test/carnivalTest.js
+++ test/carnivalTest.js
@@ -7,6 +7,30 @@ module.exports = {
     'Verify Page': function (browser) {
       carnivalHome(browser).verifyPage();
     },
+    'Click on sail to filter': function (browser) {
+      carnivalHome(browser).clickSailTo();
+    },
+    'Set SailTo value': function (browser) {
+      carnivalHome(browser).setSailToValue();
+    },
+    'Click on duration filter': function (browser) {
+      carnivalHome(browser).clickDuration();
+    },
+    'Set duration value': function (browser) {
+      carnivalHome(browser).setDuration();
+    },
+    'Assert the grid view is shown': function (browser) {
+      carnivalHome(browser).assertGridView();
+    },
+    'Move min value slider': function (browser) {
+      carnivalHome(browser).moveSliderMinToRight();
+    },
+    'Check slider function': function (browser) {
+      carnivalHome(browser).assertSliderFunction();
+    },
+    'Reset value of slider': function (browser) {
+      carnivalHome(browser).clickResetValues();
+    },
     after: function (browser) {
       browser.pause(5000);
       browser.end();
